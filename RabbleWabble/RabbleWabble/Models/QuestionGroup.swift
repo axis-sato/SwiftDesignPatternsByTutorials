718c8146b3f5983e1719ada41255191830542537
Masahiko Sato <sato_m@mediaxis.jp>
2018-08-08 22:36:09 +0900

    Implement Observer Pattern

--- RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
+++ RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -9,13 +9,40 @@
 public class QuestionGroup: Codable {
     
     public class Score: Codable {
-        public var correctCount: Int = 0
-        public var incorrectCount: Int = 0
+        public var correctCount: Int = 0 {
+            didSet {
+                updateRunningPercentage()
+            }
+        }
+        public var incorrectCount: Int = 0 {
+            didSet {
+                updateRunningPercentage()
+            }
+        }
         public init() { }
+        
+        public lazy var runningPercentage = Observable(calculateRunningPercentage())
+        
+        public func reset() {
+            correctCount = 0
+            incorrectCount = 0
+        }
+        
+        private func calculateRunningPercentage() -> Double {
+            let totalCount = correctCount + incorrectCount
+            guard totalCount > 0 else {
+                return 0
+            }
+            return Double(correctCount) / Double(totalCount)
+        }
+        
+        private func updateRunningPercentage() {
+            runningPercentage.value = calculateRunningPercentage()
+        }
     }
     
     public let questions: [Question]
-    public var score: Score
+    public private(set) var score: Score
     public let title: String
     
     public init(questions: [Question], score: Score = Score(), title: String) {
